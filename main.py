f6b5e530fe6f842ebb03f79b53d1226a4a122058
VarunP09 <152016320+VarunP09@users.noreply.github.com>
2026-09-01 21:33:08 -0400

    Uploaded for TA requirement
    As part of onboarding for CS1 TA's I have finished the CS1 final project.

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
-from bakery import assert_equal
 from drafter import *
 from dataclasses import dataclass
 
 
+set_website_title("Counter")
 
-hide_debug_information()
-set_website_title("Your Drafter Website")
+set_site_information(
+    author="Varun Pappu",
+    description="A simple counter",
+    sources="Thanks to the Drafter docs",
+    planning="N/A",
+    links=["https://github.com/your-username/your-repository"]
+)
+
 set_website_framed(False)
-
 
 @dataclass
 class State:
-    pass
+    count: int
 
 
 @route
 def index(state: State) -> Page:
-    return Page(state, ["Hello World!"])
+    return Page(state, [
+        "Current count: " + str(state.count) + "\n",
+        Button("+1", "increment"),
+        Button("-1", "decrement"),
+        Button("+5", "plusFive"),
+        Button("Reset", "reset_count")
+    ])
 
-assert_equal(index(State()), Page(State(), ["Hello World!"]))
 
-start_server(State())
+@route
+def increment(state: State) -> Page:
+    state.count = state.count + 1
+    return index(state)
+
+@route
+def decrement(state: State) -> Page:
+    if(state.count > 0):
+        state.count = state.count - 1
+    return index(state)
+
+@route
+def plusFive(state: State) -> Page:
+    state.count = state.count + 5
+    return index(state)
+
+
+@route
+def reset_count(state: State) -> Page:
+    state.count = 0
+    return index(state)
+
+
+assert_state(increment(State(0)), State(1))
+assert_state(reset_count(State(7)), State(0))
+assert_has(index(State(3)), "Current count: 3")
+
+hide_debug_information()
+
+start_server(State(5))
